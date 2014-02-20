22a00bc3e43275ce6b19b1b21e0acc02c78c54fb
Miguel Collantes <miguelcollantes1@yahoo.com>
2014-02-20 09:59:36 +0800

    added directions

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,32 @@
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
 function initialize() {
+	directionsDisplay = new google.maps.DirectionsRenderer();
 	var mapOptions = {
 	center: new google.maps.LatLng(14.5833, 120.9667),
 	zoom: 12
 	};
 	
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
+	map = new google.maps.Map(document.getElementById("map-canvas"),
 	mapOptions);
+	directionsDisplay.setMap(map);
 }
+
+function calcRoute() {
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
