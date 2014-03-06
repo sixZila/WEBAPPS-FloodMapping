e8bb6ec67447cfa5c54878655dbb8fc60d385b29
sixZila <miguelcollantes1@yahoo.com>
2014-03-06 10:57:12 +0800

    Cleaned Code, Changed UI Scheme

--- Assets/script.js
+++ Assets/script.js
@@ -23,25 +23,14 @@ function initialize() {
 	var start = /** @type {HTMLInputElement} */(document.getElementById('start'));
 	var destination = /** @type {HTMLInputElement} */(document.getElementById('end'));
 
-	var bounds = new google.maps.LatLngBounds(
-		new google.maps.LatLng(15.00, 119.00),
-		new google.maps.LatLng(13.00, 121.00)
-	);
-	
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	
 	var autocompleteStart = new google.maps.places.Autocomplete(start);
 	var autocompleteEnd = new google.maps.places.Autocomplete(destination);
 
-	autocompleteStart.setBounds(bounds);
-	autocompleteEnd.setBounds(bounds);
-	
 	directionsDisplay.setMap(map);
 	setMarkers(map, floods);
 	
-	for(var layer in layers) {
-		
-	}	
 	var kmzLayer0 = new google.maps.KmlLayer({
 			url: "http://nababaha.appspot.com/static/marikina-ondoy.KMZ"
 	});
