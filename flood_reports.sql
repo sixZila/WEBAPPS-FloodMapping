e783a167e58da794b8ce17c7437d1d53d5c1c9bb
Dannika <r.dannika@yahoo.com>
2014-04-04 13:34:26 +0800

    Added image directory and uploading time to DB

--- flood_reports.sql
+++ flood_reports.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2014 at 06:35 PM
+-- Generation Time: Apr 04, 2014 at 06:00 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.9
 
@@ -28,12 +28,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `entries` (
   `entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` text NOT NULL,
+  `image_dir` text NOT NULL,
   `longitude` decimal(9,6) NOT NULL,
   `latitude` decimal(9,6) NOT NULL,
   `level` int(11) NOT NULL,
-  `image` blob NOT NULL,
+  `upload_time` datetime NOT NULL,
   PRIMARY KEY (`entry_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
