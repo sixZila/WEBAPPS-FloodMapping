12b2cbfcc090daff147b3aec18279cf6bd32f1e9
sixZila <miguelcollantes1@yahoo.com>
2014-04-04 17:48:52 +0800

    Added Alen's Logo - Mini Challenge 2

---
+++ Miguel Collantes/assets/js/main.js
@@ -0,0 +1,48 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+	$('.with-hover-text').hover(
+		function(e) {
+			$(this).css('overflow', 'visible');
+			$(this).find('.hover-text')
+				.show()
+				.css('opacity', 0)
+				.delay(200)
+				.animate(
+					{
+						paddingTop: '25px',
+						opacity: 1
+					},
+					'fast',
+					'linear'
+				);
+		},
+		function(e) {
+			var obj = $(this);
+			$(this).find('.hover-text')
+				.animate(
+					{
+						paddingTop: '0',
+						opacity: 0
+					},
+					'fast',
+					'linear',
+					function() {
+						$(this).hide();
+						$( obj ).css('overflow', 'hidden');
+					}
+				);
+		}
+	);
+	
+	var img_loaded = 0;
+	var j_images = [];
